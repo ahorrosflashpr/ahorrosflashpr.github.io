1f5bbd9eb7372f0e6552b100a75a4f4b606ada3e
ahorrosflashpr <ahorrosydescuentosflashpr@gmail.com>
2026-07-14 11:34:36 -0700

    Update ofertas.js

--- ofertas.js
+++ ofertas.js
@@ -1,7 +1,7 @@
 const ofertas = [
 {
     nombre: "Aspiradora Inalámbrica",
-    imagen: "images/41bTOjOl-yL._AC_.jpg",
+    imagen: "images/aspiradora.jpg",
     precio: "$21.59",
     antes: "$119.99",
     descuento: "82%",
