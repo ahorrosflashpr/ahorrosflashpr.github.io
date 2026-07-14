bcef00e5485b51002d9b73e17edac0f4dbaec750
ahorrosflashpr <ahorrosydescuentosflashpr@gmail.com>
2026-07-14 13:15:36 -0700

    Update ofertas.js

--- ofertas.js
+++ ofertas.js
@@ -1,4 +1,5 @@
 const ofertas = [
+
 {
     nombre: "Aspiradora Inalámbrica",
     imagen: "images/Aspiradora.jpg",
@@ -8,6 +9,34 @@ const ofertas = [
     ahorro: "$98.40",
     categoria: "Hogar",
     etiqueta: "🔥 Oferta Flash",
-    enlace: "https://amzn.to/452dXnV"
+    destacada: true,
+    enlace: "#"
+},
+
+{
+    nombre: "Organizador de estación de café con divisores ajustables",
+    imagen: "images/Aspiradora.jpg",
+    precio: "$13.99",
+    antes: "$19.99",
+    descuento: "30%",
+    ahorro: "$6",
+    categoria: "Cocina",
+    etiqueta: "⚡ Oferta Relámpago",
+    destacada: true,
+    enlace: "https://amzn.to/4aUGeQS"
+},
+
+{
+    nombre: "Freidora sin Aceite",
+    imagen: "images/Aspiradora.jpg",
+    precio: "$49.99",
+    antes: "$129.99",
+    descuento: "62%",
+    ahorro: "$80.00",
+    categoria: "Cocina",
+    etiqueta: "⭐ Más Vendido",
+    destacada: true,
+    enlace: "#"
 }
+
 ];
