ea77e389abcdcd9d28d3c9659701ab99515e3177
ahorrosflashpr <ahorrosydescuentosflashpr@gmail.com>
2026-07-14 08:47:42 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("Ahorros y Descuentos Flash PR cargado correctamente");
+const contenedor = document.getElementById("productos");
+
+ofertas.forEach(oferta => {
+
+contenedor.innerHTML += `
+<div class="card">
+
+<img src="${oferta.imagen}" alt="${oferta.nombre}">
+
+<h2>${oferta.nombre}</h2>
+
+<p class="old">${oferta.antes}</p>
+
+<p class="price">${oferta.precio}</p>
+
+<span class="off">${oferta.descuento}</span>
+
+<a href="${oferta.enlace}" target="_blank">Ver Oferta</a>
+
+</div>
+`;
+
+});
