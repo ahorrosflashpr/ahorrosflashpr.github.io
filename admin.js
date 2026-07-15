9b9f8989bf56f01d14be92dca11c887234955b70
ahorrosflashpr <ahorrosydescuentosflashpr@gmail.com>
2026-07-14 17:29:14 -0700

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,1 +1,52 @@
+import { db } from "../firebase.js";
 
+import {
+    collection,
+    addDoc
+} from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
+
+const formulario = document.getElementById("formOferta");
+
+formulario.addEventListener("submit", async (e)=>{
+
+    e.preventDefault();
+
+    try{
+
+        await addDoc(collection(db,"ofertas"),{
+
+            nombre:document.getElementById("nombre").value,
+
+            precio:document.getElementById("precio").value,
+
+            antes:document.getElementById("antes").value,
+
+            descuento:document.getElementById("descuento").value,
+
+            ahorro:document.getElementById("ahorro").value,
+
+            categoria:document.getElementById("categoria").value,
+
+            amazon:document.getElementById("amazon").value,
+
+            joylink:document.getElementById("joylink").value,
+
+            imagen:"images/" + document.getElementById("imagen").files[0].name
+
+        });
+
+        alert("✅ Oferta publicada");
+
+        formulario.reset();
+
+    }
+
+    catch(error){
+
+        console.log(error);
+
+        alert("Error");
+
+    }
+
+});
