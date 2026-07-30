578101704db25d10da2e79127b65875dd5c61868
ahorrosflashpr <ahorrosydescuentosflashpr@gmail.com>
2026-07-29 19:21:06 -0700

    Update admin.js

--- admin.js
+++ admin.js
@@ -562,25 +562,6 @@ infoPagina.textContent = `Página ${paginaActual}`;
 
 });
 
-const btnCategoria = document.getElementById("btnCategoria");
-const menuCategorias = document.getElementById("menuCategorias");
-
-btnCategoria.addEventListener("click", () => {
-    menuCategorias.style.display =
-        menuCategorias.style.display === "block" ? "none" : "block";
-});
-
-window.seleccionarCategoria = function(valor, texto) {
-    document.getElementById("categoria").value = valor;
-    btnCategoria.innerHTML = `${texto}<span>▼</span>`;
-    menuCategorias.style.display = "none";
-};
-
-document.addEventListener("click", (e) => {
-    if (!e.target.closest(".selectorCategoria")) {
-        menuCategorias.style.display = "none";
-    }
-});
 
 window.venceManana = function(){
 
